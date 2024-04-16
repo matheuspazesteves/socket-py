d1308edb875f5f110db79d351436b64b0581942f
Matheus Paz Esteves <matheuspazesteves@hotmail.com>
2024-04-16 09:37:45 -0300

    wip

--- TCPClient.py
+++ TCPClient.py
@@ -1,11 +1,34 @@
-from socket import *
-serverName = 'localhost'
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
-sentence = input('Input lowercase sentence:')
-encoded_message = bytes(sentence, "utf-8")
-clientSocket.send(encoded_message)
-modifiedSentence = clientSocket.recv(1024)
-print ('From Server: ', modifiedSentence)
-clientSocket.close()
+import os
+import socket
+import pickle
+from dotenv import load_dotenv
+
+load_dotenv();
+
+
+def main():
+    server_ip = os.getenv("SERVER_IP")
+    server_port = int(os.getenv("SERVER_PORT"))
+
+    while True:
+        operation = input("Escolha a operação (+, -, *, /) ou 'sair' para encerrar: ")
+        if operation.lower() == 'sair':
+            break
+
+        numbers = input("Digite os números separados por espaço: ")
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        message = {
+            'operacao': operation,
+            'numeros': numbers
+        }
+        data=pickle.dumps(message)
+        client_socket.connect((server_ip, server_port))
+        client_socket.send(data)
+
+        result = client_socket.recv(1024).decode()
+        print(f"Resultado: {result}")
+
+        client_socket.close()
+
+if __name__ == "__main__":
+    main()
--- TCPServer.py
+++ TCPServer.py
@@ -1,12 +1,45 @@
-from socket import *
-serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
-print ('The server is ready to receive')
-while 1:
-    connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024)
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence)
-    connectionSocket.close()
+import os
+import socket
+import pickle
+from functools import reduce
+from dotenv import load_dotenv
+load_dotenv();
+
+def handle_client(client_socket):
+    request = client_socket.recv(1024)
+    data = pickle.loads(request)
+    numbers = list(map(float, data['numeros'].split()))
+    operation = data['operacao']
+
+    if operation == '+':
+        result = sum(numbers)
+    elif operation == '-':
+        result = reduce(lambda x, y: x - y, numbers)
+    elif operation == '*':
+        result = 1
+        for num in numbers:
+            result *= num
+    elif operation == '/':
+        if numbers[1] == 0:
+            result = "Impossivel dividir por 0"
+        else:
+            result = numbers[0] / numbers[1]
+    else:
+        result = "Operação inválida"
+
+    client_socket.send(str(result).encode())
+    client_socket.close()
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((os.getenv("SERVER_LOCAL"), int(os.getenv("SERVER_PORT"))))
+    server.listen(5)
+    print("Servidor da Calculadora iniciado. Aguardando conexões...")
+
+    while True:
+        client_socket, addr = server.accept()
+        print(f"Conexão recebida de {addr}")
+        handle_client(client_socket)
+
+if __name__ == "__main__":
+    main()
